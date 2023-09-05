0731c0a5b83957d7ea39129a7f3217e5dec6dc80
rafaelsuporte99@hotmail.com <rafaelsuporte99@hotmail.com>
2023-09-05 18:40:37 -0300

    ajustes

--- src/Contador.java
+++ src/Contador.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class Contador {
     public static void main(String[] args) throws ParametrosInvalidosException {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Digite o primeiro parâmetro");
+        System.out.println("Digite o primeiro parametro");
         int parm1 = sc.nextInt();
-        System.out.println("Digite o segundo parâmetro");
+        System.out.println("Digite o segundo parametro");
         int parm2 = sc.nextInt();
         try{
            contar(parm1, parm2);
