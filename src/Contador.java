66c582ecd57858ddd422ff5853aa6e3737cfba2b
rafaelsuporte99@hotmail.com <rafaelsuporte99@hotmail.com>
2023-09-05 18:40:23 -0300

    finalização do projeto

---
+++ src/Contador.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) throws ParametrosInvalidosException {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro");
+        int parm1 = sc.nextInt();
+        System.out.println("Digite o segundo parâmetro");
+        int parm2 = sc.nextInt();
+        try{
+           contar(parm1, parm2);
+            
+        }catch(ParametrosInvalidosException e){
+            System.out.println(e.getMessage());
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        
+    }
+    private static void contar(int parm1,int parm2) throws ParametrosInvalidosException{
+        if(parm1 > parm2){
+            throw new ParametrosInvalidosException();
+        }
+        int cont = parm2 - parm1;
+
+        for(int i=1;i<=cont;i++){
+            System.out.println("Imprimindo o numero " + i);
+        }
+    }
+}
